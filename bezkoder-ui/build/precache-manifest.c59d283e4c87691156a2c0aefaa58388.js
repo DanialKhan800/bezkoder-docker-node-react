97b5c4cb79d87e718d78f2e9821aad7c962c2416
Muhammad Danial Khan <danialkhan800@gmail.com>
2023-01-04 15:36:11 +0500

    new addtion buildspec.yml

---
+++ bezkoder-ui/build/precache-manifest.c59d283e4c87691156a2c0aefaa58388.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1524f9a75a448cf5d98b623d82694bfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "761a855f810c233d7e49",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "e6e3a9c7f1c4e80414a5",
+    "url": "/static/css/main.b5fec8d3.chunk.css"
+  },
+  {
+    "revision": "761a855f810c233d7e49",
+    "url": "/static/js/2.3f37d184.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.3f37d184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e3a9c7f1c4e80414a5",
+    "url": "/static/js/main.b8cf46a7.chunk.js"
+  },
+  {
+    "revision": "f8e341d40e8ec465a0c9",
+    "url": "/static/js/runtime-main.45d7c6c0.js"
+  }
+]);
